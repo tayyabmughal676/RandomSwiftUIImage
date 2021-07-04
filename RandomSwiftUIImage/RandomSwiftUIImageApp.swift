1d4a389a2317dca0c9b31e4d912c66bbc6b245a0
tayyabmughal676 <tayyabmughal676@gmail.com>
2021-07-04 23:09:25 +0500

    Initial Commit

---
+++ RandomSwiftUIImage/RandomSwiftUIImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomSwiftUIImageApp.swift
+//  RandomSwiftUIImage
+//
+//  Created by Thor on 04/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct RandomSwiftUIImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
